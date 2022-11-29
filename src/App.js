7d6a1cbc03f97daa78b863cd733c727eedc535c0
Sijun <sionwer4@naver.com>
2022-11-29 20:50:19 +0900

    feat: first setup for To Do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("once work.");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length >= 5){
-      console.log("Search for", keyword);
+  const [toDo, setToDo]=useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
     }
-  }, [keyword]);
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  }
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <h1>My To Do ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do"/>
+        <button>Add To Do</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
