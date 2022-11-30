631c530363e120c5a704b6241d3be6f74158ac84
Sijun <sionwer4@naver.com>
2022-12-01 04:30:52 +0900

    feat: router add

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
-import { useState, useEffect } from "react";
-import Movie from "./Movie.js";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return(
+  <Router>
+    <Switch>
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+  )
 }
 
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/movie_detail.json?movie_id=${id}`
+    );
+    const json = await response.json();
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return <h1>Detail</h1>;
+}
+export default Detail;
