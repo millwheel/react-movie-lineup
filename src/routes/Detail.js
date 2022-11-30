cea3ee8e57ba506a4f5224be15164d4b45a8e352
Sijun <sionwer4@naver.com>
2022-12-01 04:46:25 +0900

    feat: detail add

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,37 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [detail, setDetail] = useState([]);
   const { id } = useParams();
   const getMovie = async () => {
     const response = await fetch(
-      `https://yts.mx/api/v2/movie_detail.json?movie_id=${id}`
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
     );
     const json = await response.json();
+    console.log(json);
+    setDetail(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={detail.background_image_original} />
+          <h1>{detail.title}</h1>
+          <p>{detail.description_full}</p>
+          <ul>rating: {detail.rating}</ul>
+          <ul>download: {detail.download_count}</ul>
+          <ul>year: {detail.year}</ul>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
